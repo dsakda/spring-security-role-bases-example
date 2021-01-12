238419966ce0fa3031e07b386a461ec503004de5
dsakda <74044765+dsakda@users.noreply.github.com>
2021-01-12 21:28:07 +0700

    Update WebSecurityConfig.java

--- src/main/java/com/example/config/WebSecurityConfig.java
+++ src/main/java/com/example/config/WebSecurityConfig.java
@@ -1,18 +1,33 @@
 package com.example.config;
 
+import com.example.jwt.AuthEntryPointJwt;
+import com.example.jwt.AuthTokenFilter;
 import com.example.service.CustomerDetailsServiceImpl;
 import com.example.util.CustomerRole;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
 import org.springframework.security.authentication.dao.*;
 import org.springframework.security.config.annotation.authentication.builders.*;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
+    }
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -40,7 +55,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+/*        http.authorizeRequests()
                 .antMatchers("/").hasAnyAuthority(CustomerRole.USER.getRole(), CustomerRole.CREATOR.getRole(), CustomerRole.EDITOR.getRole(), CustomerRole.ADMIN.getRole())
                 .antMatchers("/new").hasAnyAuthority(CustomerRole.ADMIN.getRole(), CustomerRole.CREATOR.getRole())
                 .antMatchers("/edit/**").hasAnyAuthority(CustomerRole.ADMIN.getRole(), CustomerRole.EDITOR.getRole())
@@ -51,7 +66,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout().permitAll()
                 .and()
-                .exceptionHandling().accessDeniedPage("/403")
-        ;
+                .exceptionHandling().accessDeniedPage("/403");*/
+
+        http.cors().and().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests().antMatchers("/api/auth/**").permitAll()
+                .antMatchers("/api/test/**").permitAll()
+                .anyRequest().authenticated();
+
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
