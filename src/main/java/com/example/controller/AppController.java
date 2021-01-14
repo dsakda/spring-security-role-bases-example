9d85013c647eb0d02fc08a27874c944daad878fb
dsakda <74044765+dsakda@users.noreply.github.com>
2021-01-15 06:06:12 +0700

    Update AppController.java

--- src/main/java/com/example/controller/AppController.java
+++ src/main/java/com/example/controller/AppController.java
@@ -1,13 +1,36 @@
 package com.example.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/content")
 public class AppController {
 
-    @GetMapping
-    public String welcomePage() {
-        return "index";
+    @GetMapping("/all")
+    public String allAccess() {
+        return "All Access.";
+    }
+
+    @GetMapping("/creator")
+    @PreAuthorize("hasRole('CREATOR') or hasRole('ADMIN')")
+    public String creatorAccess() {
+        return "Creator Access.";
+    }
+
+    @GetMapping("/editor")
+    @PreAuthorize("hasRole('EDITOR') or hasRole('ADMIN')")
+    public String editorAccess() {
+        return "Editor Access.";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminAccess() {
+        return "Admin Access.";
     }
 }
